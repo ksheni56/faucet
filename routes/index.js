099aab5a6ee041f3b12c1013985aaa9f82e281e1
reymalahay <reymalahay@gmail.com>
2018-06-21 22:09:04 -0600

    VIT 21: rebrand and redeploy the signup process on a separate digital ocean instance
    Removed Steemit text.

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/.well-known/healthcheck.json', (req, res) => {
 });
 
 router.get('/*', (req, res) => {
-    res.render('index', { title: 'Sign up on Steem' });
+    res.render('index', { title: 'Sign up on ViceTube' });
 });
 
 module.exports = router;
